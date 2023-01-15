7aa678dc0e2a584d7c802d48f6fa74b6cdf9c002
Nand Raj <nandraj2001@gmail.com>
2023-01-16 03:16:27 +0530

    added coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Button, Container, Form } from 'react-bootstrap';
 
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log("Email:", e.target.email.value);
+    // sendEmailToBackend
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Container>
+        <div className="content">
+          COMING SOON
+        </div>
+        <div className="info">Our developers are working on the website</div>
+        <div className="info">Get notified when the app is launched</div>
+        <Form onSubmit={handleSubmit} className="formWrapper" >
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Control type="email" placeholder="Email address" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Notify Me
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+function CollapsibleExample({ setPage }) {
+    const pages = ['Home', 'About Us', 'Contact'];
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+            <Container>
+                <Navbar.Brand href='/'>Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav fill variant="pills" className="me-auto" defaultActiveKey="/Home">
+                        {pages.map((page) => (
+                            <Nav.Link
+                                href={`/${page}`}
+                                key={page}
+                                onClick={() => { setPage(page) }}
+                            >
+                                {page}
+                            </Nav.Link>
+                        ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default CollapsibleExample;
